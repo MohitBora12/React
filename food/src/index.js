c0db93a5c05df604577f33d5e65371bbebd4b6fb
Mohit Bora <boramohit3@gmail.com>
2024-08-27 22:23:53 +0530

    Made the structure

--- food/src/index.js
+++ food/src/index.js
@@ -30,10 +30,49 @@ const Header = () => {
         
     );
 };
+
+const burgerking = {
+
+    name: "Burger King",
+    img: "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/RX_THUMBNAIL/IMAGES/VENDOR/2024/6/11/55d0daa1-ad2e-4893-be06-4709c5c68d49_41350.JPG",
+    cusines: ["Burger", "American"],
+    rating: "4.2",
+
+}
+
+const RestrauntCard = () => {
+    
+    return (
+
+        <div className='card'>
+            <img src={burgerking.img}></img>
+            <h2>{burgerking.name}</h2>
+            <h3>{burgerking.cusines.join(', ')}</h3>
+            <h4>{burgerking.rating} stars</h4>
+        </div>
+    );
+};
 const Body = () => {
     
     return (
-        <h4>BODY</h4>
+        <div className='Restraunt-list'>
+            <RestrauntCard/>
+            <RestrauntCard/>
+            <RestrauntCard/>
+            <RestrauntCard/>
+            <RestrauntCard/>
+            <RestrauntCard/>
+            <RestrauntCard/>
+            <RestrauntCard/>
+            <RestrauntCard/>
+            <RestrauntCard/>
+            <RestrauntCard/>
+            <RestrauntCard/>
+            <RestrauntCard/>
+            <RestrauntCard/>
+            <RestrauntCard/>
+            <RestrauntCard/>
+        </div>
     );
 };
 const Footer = () => {
