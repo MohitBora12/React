634b9130aa69794dcec01bcecb9d0021f216084c
Mohit Bora <boramohit3@gmail.com>
2024-08-27 16:44:07 +0530

    made the header structure

--- food/src/index.js
+++ food/src/index.js
@@ -2,3 +2,55 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 
+import img from "../src/logo.jpg"
+
+const Title = () => (
+    
+    <a href="/">
+
+    <img className="logo" alt="logo" src={img} />
+    </a>
+);
+
+const Header = () => {
+
+    return (
+        <div className='header'>
+            <Title /> 
+            <div className='nav-items'>
+
+            <ul>
+                <li>Home</li>
+                <li>About</li>
+                <li>Contact</li>
+                <li>Cart</li>
+            </ul>
+            </div>
+        </div>
+        
+    );
+};
+const Body = () => {
+    
+    return (
+        <h4>BODY</h4>
+    );
+};
+const Footer = () => {
+    
+    return (
+        <h4>Footer</h4>
+    );
+};
+
+const AppLayout = () => {
+    return (
+        <>
+            <Header />
+            <Body />
+            <Footer />
+        </>  
+  );
+};
+const root = ReactDOM.createRoot(document.getElementById("root"))
+root.render(<AppLayout />)
